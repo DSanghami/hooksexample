4a92aece9be76c7f4407279cc0d498aeda30309f
sanghami.d <sanghami.d@gavstech.com>
2023-10-30 14:08:49 +0530

    commit change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import logo from './logo.svg';
+
 import './App.css';
 
+import { useState , useEffect} from 'react';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+ 
+
+  const [visitorCount , setVisitorCount ] = useState(1);
+  //tHIS ARROW WILL EXECUTE in all 3 phases, viz, Mounting
+  // Updting and Unmounting phases, useEffect hook introduces all
+  //the 3 phases for this functional component.
+  
+  const displayStatus = ()=>{
+    console.log("Visitor count incremented"  + visitorCount);
+    //setVisitorCount(visitorCount + 1);
+    
+};
+
+// ReactJS implements an observer which observes the changes for the 
+// visitorCount and if visitorCount changes, it automatically calls
+// that function
+useEffect(displayStatus, [visitorCount])
+  const updateVisitorCount = () =>
+
+  {
+
+    setVisitorCount(visitorCount+1);
+
+  }
+
+   return (
+
+    <center>
+
+  <div className="App">
+
+      <form>
+
+        <input type='button' onClick={updateVisitorCount} value="update Vistor" /> <br />
+
+        <b>You are Vistor </b> {visitorCount}
+
+      </form>
+
     </div>
+
+    </center>
+
+ 
+
   );
+
 }
 
+ 
+
 export default App;
+
+ 
